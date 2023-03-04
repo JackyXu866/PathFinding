04e2c0fe7dd1d577eaf83d8c04420645b6b62eee
jackyxu866 <33426411+JackyXu866@users.noreply.github.com>
2023-03-04 08:26:30 -0500

    a* tile

--- Assets/Scripts/LoadMap.cs
+++ Assets/Scripts/LoadMap.cs
@@ -4,32 +4,123 @@ using UnityEngine;
 using System.Text.RegularExpressions;
 using System.Linq;
 using System;
+using TMPro;
+using UnityEngine.EventSystems;
+
 
 public class LoadMap : MonoBehaviour
 {
+    [SerializeField] TMP_Dropdown dropdown_Map, dropdown_Algorithm;
     public TextAsset[] mapFiles;
     public GameObject[] mapTiles;
     Hashtable mapTable = new Hashtable();
+    Node startNode, endNode, pendingNode;
 
     public int mapWidth, mapHeight;
+    static Func<Node, Node, float> heuristic = null;
 
     // Start is called before the first frame update
     void Start()
     {
         mapFiles = Resources.LoadAll<TextAsset>("Maps");
         mapTiles = Resources.LoadAll<GameObject>("Tiles");
+
+        // dropdown_Map
+        dropdown_Map.ClearOptions();
+        List<string> options = new List<string>();
+        foreach(TextAsset map in mapFiles)
+        {
+            options.Add(map.name);
+        }  
+        dropdown_Map.AddOptions(options);
+        dropdown_Map.onValueChanged.AddListener(delegate {
+            MapCreate(dropdown_Map.value);
+        });
+        MapCreate(0);
+
+        // dropdown_Algorithm
+        dropdown_Algorithm.ClearOptions();
+        options = new List<string>();
+        options.Add("Manhattan");
+        options.Add("Euclidean");
+        dropdown_Algorithm.AddOptions(options);
+        dropdown_Algorithm.onValueChanged.AddListener(delegate {
+            switch(dropdown_Algorithm.value){
+                case 0:
+                    heuristic = Heuristic_Manhattan;
+                    break;
+                case 1:
+                    heuristic = Heuristic_Manhattan;
+                    break;
+            }
+        });
+        heuristic = Heuristic_Manhattan;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.inputString != ""){
-            int number;
-            bool is_a_number = Int32.TryParse(Input.inputString, out number);
-            if (is_a_number && number-1 >= 0 && number-1 < mapFiles.Length){
-                MapCreate(number-1);
+        if(EventSystem.current.IsPointerOverGameObject())
+            return;
+        if(pendingNode != null){
+            if(Input.GetKeyDown(KeyCode.S)){
+                // start 
+                if(startNode != null)
+                    startNode.SetColor(Color.white);
+                startNode = pendingNode;
+                pendingNode = null;
+                startNode.SetColor(Color.red);
+            }
+            else if(Input.GetKeyDown(KeyCode.E)){
+                // end
+                if(endNode != null)
+                    endNode.SetColor(Color.white);
+                endNode = pendingNode;
+                pendingNode = null;
+                endNode.SetColor(Color.green);
+            }
+            else if(Input.GetKeyDown(KeyCode.O)){
+                // obstacle
+                if(pendingNode == startNode || pendingNode == endNode){}
+                else{
+                    GameObject tmp = Instantiate(mapTiles[1], pendingNode.tile.transform.position, Quaternion.identity);
+                    tmp.transform.parent = transform;
+                    Destroy(pendingNode.tile);
+                    pendingNode.ChangeTile(tmp);
+                    pendingNode = null;
+                }
             }
         }
+
+        if(Input.GetButtonDown("Fire1")){
+            Vector3 mousePos = Input.mousePosition;
+            {
+                Ray ray = Camera.main.ScreenPointToRay(mousePos);
+                RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+                if(hit.collider.CompareTag("Walkable"))
+                {
+                    int x = Mathf.FloorToInt(hit.collider.transform.position.x);
+                    int y = Mathf.FloorToInt(hit.collider.transform.position.y);
+                    Node tmp = (Node)mapTable[new Vector2(x, y)];
+                    if(tmp == null || tmp == startNode || tmp == endNode){}
+                    else if(pendingNode == null){
+                        pendingNode = tmp;
+                        pendingNode.SetColor(Color.grey);
+                    }
+                    else if(tmp != pendingNode){
+                        pendingNode.SetColor(Color.white);
+                        pendingNode = tmp;
+                        pendingNode.SetColor(Color.grey);
+                    }
+                    else if(pendingNode == tmp){
+                        pendingNode.SetColor(Color.white);
+                        pendingNode = null;
+                    }
+                }
+
+            }
+        }
+
     }
 
     private byte[][] ParseMapFile(int index)
@@ -91,13 +182,79 @@ public class LoadMap : MonoBehaviour
                 GameObject tiletmp = Instantiate(mapTiles[tile], new Vector3(x, y, 0), Quaternion.identity);
                 tiletmp.transform.parent = transform;
                 Node node = new Node();
-                node.tile = tiletmp;
+                node.ChangeTile(tiletmp);
                 mapTable.Add(new Vector2(x, y), node);
                 node.AddNeighbor(ref mapTable, x, y);
             }
         }
 
         Camera.main.transform.position = new Vector3(mapWidth/2, mapHeight/2, -10);
+        Camera.main.orthographicSize = Mathf.Min(mapWidth, mapHeight)/4;
+    }
+
+    public void StartAStar(){
+        StartCoroutine(Astar_tile());
+    }
+
+    IEnumerator Astar_tile(){
+        if(startNode == null || endNode == null)
+            yield return null;
+        List<Node> openList = new List<Node>();
+        List<Node> closedList = new List<Node>();
+        openList.Add(startNode);
+        
+        while(openList.Count > 0){
+            Node q = null;
+            foreach (Node n in openList){
+                if(q == null || n.fCost < q.fCost){
+                    q = n;
+                }
+            }
+            openList.Remove(q);
+
+            foreach(Node n in q.neighbors){
+                if(n.tile.CompareTag("Obstacle"))
+                    continue;
+                if(n == endNode){
+                    endNode.parent = q;
+                    Debug.Log("Found path");
+                    yield return StartCoroutine(RetracePath());
+                    yield break;
+                }
+                float newGCost = q.gCost + heuristic(n, q);
+                float newHCost = heuristic(n, endNode);
+                float newFCost = newGCost + newHCost;
+
+                if(openList.Contains(n) && n.fCost < newFCost)
+                    continue;
+                if(closedList.Contains(n) && n.fCost < newFCost)
+                    continue;
+
+                n.gCost = newGCost;
+                n.hCost = newHCost;
+                n.fCost = newFCost;
+                n.parent = q;
+
+                if(!openList.Contains(n)){
+                    openList.Add(n);
+                    n.SetColor(Color.blue);
+                }
+            }
+            closedList.Add(q);
+            q.SetColor(Color.yellow);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+
+    }
+
+    IEnumerator RetracePath(){
+        Node tmp = endNode;
+        while(tmp != startNode){
+            tmp.SetColor(Color.red);
+            tmp = tmp.parent;
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 
     int GetTileNum(char tile)
@@ -122,5 +279,15 @@ public class LoadMap : MonoBehaviour
             Destroy(child.gameObject);
         }
         mapTable.Clear();
+    }
+
+    static float Heuristic_Manhattan(Node a, Node b)
+    {
+        return Mathf.Abs(a.x - b.x) + Mathf.Abs(a.y - b.y);
+    }
+
+    static float Heuristic_Euclidean(Node a, Node b)
+    {
+        return Mathf.Sqrt(Mathf.Pow(a.x - b.x, 2) + Mathf.Pow(a.y - b.y, 2));
     }
 }
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Node Color: white: normal, red: start, green: end, blue: open, yellow: closed, grey: pending
 public class Node
 {
     public GameObject tile;
     public List<Node> neighbors = new List<Node>();
+    public int x, y;
+    public float gCost=0, hCost=0, fCost=0;
+    public Node parent;
+
+    public void ChangeTile(GameObject tile){
+        this.tile = tile;
+        x = (int)tile.transform.position.x;
+        y = (int)tile.transform.position.y;
+    }
 
     // only add whats created and add `this` to them as well
     public void AddNeighbor(ref Hashtable mapTable, int x, int y)
@@ -31,5 +42,48 @@ public class Node
             ((Node)mapTable[new Vector2(x+1, y-1)]).neighbors.Add(this);
         }
 
+    }
+
+    public void SetColor(Color color)
+    {
+        Color currentColor = tile.GetComponent<SpriteRenderer>().color;
+        if((color == Color.blue || color == Color.yellow)
+        && (currentColor == Color.red || currentColor == Color.green)){
+            return;
+        }
+        tile.GetComponent<SpriteRenderer>().color = color;
+    }
+
+    public static bool operator ==(Node a, Node b)
+    {
+        if((object)a == null && (object)b == null)
+            return true;
+        else if ((object)a == null || (object)b == null)
+            return false;
+        return a.tile == b.tile;
+    } 
+
+    public static bool operator !=(Node a, Node b)
+    {
+        if((object)a == null && (object)b == null)
+            return false;
+        else if ((object)a == null || (object)b == null)
+            return true;
+        return a.tile != b.tile;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if(obj == null)
+            return false;
+        Node node = obj as Node;
+        if((object)node == null)
+            return false;
+        return tile == node.tile;
+    }
+    
+    public override int GetHashCode()
+    {
+        return tile.GetHashCode();
     }
 }
