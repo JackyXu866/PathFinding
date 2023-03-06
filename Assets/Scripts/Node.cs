e84fa2f7b55240edafa583c4d0a069503d66ab69
Dongsheng_Lyu <77319993+CodelifeLyu@users.noreply.github.com>
2023-03-05 20:54:49 -0600

    AddWaypointConnection update

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -45,12 +46,32 @@ public class Node
     }
 
     //add pointway neighbors
-    public void AddPointwayConnection(ref Hashtable mapTable, Node targetNode)
+    public void AddWaypointConnection(ref Hashtable table, Node currentNode, Node targetNode)
     {
         //check if there is obstacle between current node and target node
         //use y = ax + b function and round up to see result
         //(0,0) --> (3,1)
         //check (0,0)(1,0)(2,1)(3,1)
+        //float a = float(currentNode.y - targetNode.y) / float(currentNode.x - targetNode.x);
+        //float b = a * currentNode.x - currentNode.y;
+        for (int i = currentNode.x + 1; i < targetNode.x; i++)
+        {
+            for (int j = currentNode.y + 1; j < targetNode.y; j++)
+            {
+                if(LinePassesThroughGrid((float)currentNode.x, (float)currentNode.y, (float)targetNode.x, (float)targetNode.y, (float)i, (float)j, (float)1.0))
+                {
+                    Vector2 position = new Vector2(i, j);
+                    Collider2D collider = Physics2D.OverlapPoint(position);
+                    GameObject gameObject = collider.gameObject;
+                    if (gameObject.CompareTag("obstacle"))
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+        ((Node)table[new Vector2(currentNode.x, currentNode.y)]).neighbors.Add(targetNode);
+        ((Node)table[new Vector2(targetNode.x, targetNode.y)]).neighbors.Add(currentNode);
     }
 
     public void SetColor(Color color)
@@ -94,5 +115,28 @@ public class Node
     public override int GetHashCode()
     {
         return tile.GetHashCode();
+    }
+
+    public bool LinePassesThroughGrid(float x1, float y1, float x2, float y2, float gridCenterX, float gridCenterY, float gridSize)
+    {
+        float halfGridSize = gridSize / 2f;
+        float xMin = Math.Min(x1, x2) - gridCenterX;
+        float xMax = Math.Max(x1, x2) - gridCenterX;
+        float yMin = Math.Min(y1, y2) - gridCenterY;
+        float yMax = Math.Max(y1, y2) - gridCenterY;
+
+        if (xMin < -halfGridSize && xMax > halfGridSize) return false; // Line is outside grid horizontally
+        if (yMin < -halfGridSize && yMax > halfGridSize) return false; // Line is outside grid vertically
+        if (xMin >= halfGridSize || xMax <= -halfGridSize) return false; // Line is entirely outside grid horizontally
+        if (yMin >= halfGridSize || yMax <= -halfGridSize) return false; // Line is entirely outside grid vertically
+        if (xMax - xMin == 0) return false; // Line is vertical and doesn't intersect with grid
+
+        float slope = (y2 - y1) / (x2 - x1);
+        float yAtX0 = y1 + slope * (0 - x1);
+        float yAtX1 = y1 + slope * (gridSize - x1);
+
+        if (yAtX0 >= -halfGridSize && yAtX0 <= halfGridSize) return true; // Line intersects with grid at x=0
+        if (yAtX1 >= -halfGridSize && yAtX1 <= halfGridSize) return true; // Line intersects with grid at x=gridSize
+        return false; // Line doesn't intersect with grid
     }
 }
